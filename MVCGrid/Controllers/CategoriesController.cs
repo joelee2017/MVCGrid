fdd6cf62686bce457c36c43edef962ff07aaa003
joelee2017 <as955133@gmail.com>
2018-02-13 10:32:56 +0800

    製作create update delete index檢視

--- MVCGrid/Controllers/CategoriesController.cs
+++ MVCGrid/Controllers/CategoriesController.cs
@@ -41,84 +41,95 @@ namespace MVCGrid.Controllers
         }
     
 
-        // GET: Categories/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Categories/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Categories/Create
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "CategoryID,CategoryName,Description,Picture")] Categories categories)
         {
             if (ModelState.IsValid)
             {
                 db.Categories.Add(categories);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Json(new { Result = "OK", Record = categories });
             }
 
-            return View(categories);
+            else
+            {
+                return Json(new { Result = "Error", Message="新增記錄失敗!" });
+            }
         }
 
-        // GET: Categories/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Categories categories = db.Categories.Find(id);
-            if (categories == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categories);
-        }
+        //// GET: Categories/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categories categories = db.Categories.Find(id);
+        //    if (categories == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categories);
+        //}
 
         // POST: Categories/Edit/5
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName,Description,Picture")] Categories categories)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Update([Bind(Include = "CategoryID,CategoryName,Description,Picture")] Categories categories)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(categories).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Json(new { Result = "OK", Record = categories });
             }
-            return View(categories);
+            else
+            {
+                return Json(new { Result = "Error", Message = "資料不符合規定!" });
+            }
         }
 
-        // GET: Categories/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Categories categories = db.Categories.Find(id);
-            if (categories == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categories);
-        }
+        //// GET: Categories/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categories categories = db.Categories.Find(id);
+        //    if (categories == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categories);
+        //}
 
         // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        //[HttpPost, ActionName("Delete")]
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Delete(int categoryID)
         {
-            Categories categories = db.Categories.Find(id);
+            Categories categories = db.Categories.Find(categoryID);
+            if(categories==null)
+            {
+                return Json(new { Result = "Error", Message = "找不到欲刪除的記錄!" });
+            }
             db.Categories.Remove(categories);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return Json(new { Result = "OK", Record = categories });
         }
 
         protected override void Dispose(bool disposing)
