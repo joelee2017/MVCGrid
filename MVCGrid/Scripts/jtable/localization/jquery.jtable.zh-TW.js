cf9d199171d41b4e253159e7c6d4fff4f6130c4e
joelee2017 <as955133@gmail.com>
2018-02-13 11:33:44 +0800

    多國語系自行建立，更改內容
    <script src="~/Scripts/jtable/localization/jquery.jtable.zh-TW.js"></script>

---
+++ MVCGrid/Scripts/jtable/localization/jquery.jtable.zh-TW.js
@@ -0,0 +1,30 @@
+/*
+    jTable localization file for 'Chinese' language.
+    Author: monkeycraps
+*/
+(function ($) {
+
+    $.extend(true, $.hik.jtable.prototype.options.messages, {
+        serverCommunicationError: '伺服器請求錯誤。',
+        loadingMessage: '加載中...',
+        noDataAvailable: '没有數據！',
+        addNewRecord: '創建',
+        editRecord: '編輯',
+        areYouSure: '確定？',
+        deleteConfirmation: '確定刪除這行數據？',
+        save: '保存',
+        saving: '保存中',
+        cancel: '取消',
+        deleteText: '删除',
+        deleting: '删除中',
+        error: '錯誤',
+        close: '關閉',
+        cannotLoadOptionsFor: '無法加載對象屬性 {0}',
+        pagingInfo: '顯示 {0} 至 {1} 共 {2}',
+        canNotDeletedRecords: '删除失败 {0} 至 {1}!',
+        deleteProggress: '正在删除 {0} 至 {1} 記錄, 進行中...',
+        pageSizeChangeLabel: '每頁筆數', //New. Must be localized.
+        gotoPageLabel: '前往頁面' //New. Must be localized.
+    });
+
+})(jQuery);
